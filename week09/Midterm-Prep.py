6913ca8d4d8b733a31e872e5270431722a11cca7
MMallberg <mmallber@uwyo.edu>
2024-10-22 03:46:36 +0000

    done

---
+++ week09/Midterm-Prep.py
@@ -0,0 +1,12 @@
+#10/21/24
+# ChatGPT used to generate this practice problem:
+
+def check_even_odd(number):
+    """used to check if an input integer is divisible by two or not"""
+    if number%2==0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+
+check_even_odd(4)
+check_even_odd(17)
